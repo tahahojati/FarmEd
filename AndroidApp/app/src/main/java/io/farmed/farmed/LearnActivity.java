a2323ebf834437c9245fa23a1617f6703a7d9d08
Taha <taha.hojati@gmail.com>
2018-03-04 15:10:51 -1000

    Initial commit

---
+++ AndroidApp/app/src/main/java/io/farmed/farmed/LearnActivity.java
@@ -0,0 +1,12 @@
+package io.farmed.farmed;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LearnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
